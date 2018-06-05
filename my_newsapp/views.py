1a6d385ae5b714df0d76a07a6d691aa4a746e637
Luka Ribaric <ribaluka@gmail.com>
2018-06-05 15:50:38 +0200

    Show Categories/Articles in navbar

--- my_newsapp/views.py
+++ my_newsapp/views.py
@@ -1,13 +1,13 @@
 from django.views.generic.list import ListView
  
-from .models import Category, Article
+from .models import  Article
 
 class HomeView(ListView):
     context_object_name = "article_list"
     template_name = 'my_newsapp/home.html'
 
     def get_queryset(self):
-        return Article.objects.all()
+        return Article.objects.all().order_by('-pub_date')
 
         
     
