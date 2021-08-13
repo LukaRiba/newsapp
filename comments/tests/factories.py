0997da7695636fb7b65b8a6863adc2c18acd602b
LukaRiba <35847721+LukaRiba@users.noreply.github.com>
2021-08-13 11:39:24 +0200

    Sem test (#15)
    * Use Single job starter workflow
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update semaphore configuration.
    
    * Update Semaphore configuration
    
    * Update semaphore configuration.
    
    * Fix comments.tests.factories.py.
    
    * Update semaphore configuration (fixed issue with DATABASE_NAME - not needed for running tests, issue was in comments.factories implementation).
    
    * Update Semaphore configuration - DATABASE_NAME env var is needed
    
    * Update semaphore configuration - add 'Deployment Checklist' block.
    
    * Update semaphore configuration - set DATABASE_NAME as global env var.
    
    * Update semaphore configuration.
    
    * Update semaphore configuration.
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Update Semaphore configuration
    
    * Add deploy.sh file.
    
    * testing semaphore trigger.
    
    * testing semaphore trigger [ci skip]
    
    * testing semaphore trigger
    
    * testing semaphore trigger
    
    * Test semaphore trigger.
    
    * Test semaphore trigger on newly created branch commit push.
    
    Co-authored-by: Luka Ribarić <luka.ribaric@fwd.hr>

--- comments/tests/factories.py
+++ comments/tests/factories.py
@@ -6,14 +6,14 @@ from comments.models import Comment
 from my_newsapp.tests.factories import UserFactory
 
 class CommentFactory(factory.django.DjangoModelFactory):
-    
+
     class Meta:
         model = Comment
 
     author = factory.SubFactory(UserFactory)
     text = factory.Faker('text', max_nb_chars=50)
     pub_date = factory.Faker('past_datetime', tzinfo=timezone.utc)
-    content_type = ContentType.objects.get(model='article')
+    content_type = factory.LazyAttribute(lambda obj: ContentType.objects.get(model='article'))
     object_id = None # must be set in CommentFactory constructor
     # region comment
         # Gets content_object through content_type and object_id after creation of comment (Lazy attribute).
@@ -21,21 +21,17 @@ class CommentFactory(factory.django.DjangoModelFactory):
         # if we create comment passing some article id as object_id:
         #   comment = CommentFactory(object_id=article_instance.id),
         # content_object will not be assigned and in our tests when we call article_instance.comments.all()
-        # we get empty queryset! If we print comment.content_object we get 'log entry' -> so we se that no object is 
+        # we get empty queryset! If we print comment.content_object we get 'log entry' -> so we se that no object is
         # assigned to thet field. When working with real model, content_object is assigned because
         # content_object is GenericForeignKey field, so we must also assign it here like this.
     # endregion
     content_object = factory.LazyAttribute(lambda obj: obj.content_type.get_object_for_this_type(pk=obj.object_id))
 
-class ReplyFactory(factory.django.DjangoModelFactory):
-    
+
+class ReplyFactory(CommentFactory):
+
     class Meta:
         model = Comment
 
-    author = factory.SubFactory(UserFactory)
-    text = factory.Faker('text', max_nb_chars=50)
     pub_date = factory.Faker('past_datetime', start_date='-30d', tzinfo=timezone.utc)
-    content_type = ContentType.objects.get(model='article')
-    object_id = None # must be set in ReplyFactory constructor
-    content_object = factory.LazyAttribute(lambda obj: obj.content_type.get_object_for_this_type(pk=obj.object_id))
     parent = factory.SubFactory(CommentFactory)
